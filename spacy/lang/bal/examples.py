f6640c77a869fe424de06e4ebe1cb2b51d5595f3
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-06-08 21:28:37 +0200

    add balochi skeleton

---
+++ spacy/lang/bal/examples.py
@@ -0,0 +1,9 @@
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.bal.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = ["TO BE INSERTED"]
